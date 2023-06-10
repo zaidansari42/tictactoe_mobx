8460067ac2660b2ab0582f962688a7cc4d74d8d4
Zaid Ansari <zaid.ansari@nonstopio.com>
2023-06-10 20:15:46 +0530

    Changed AppBar title

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tic Tac Toe Game',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(title: 'Tic Tac Toe'),
     );
   }
 }
