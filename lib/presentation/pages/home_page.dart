a8ba8dadfc4405656943e43d4f07fa21f8de2a65
Zaid Ansari <zaid.ansari@nonstopio.com>
2023-06-11 17:59:20 +0530

    Added a bug fix

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -44,50 +44,43 @@ class _HomePageState extends State<HomePage> {
         data[0] == data[2] &&
         data[0] != '') {
       _showMyDialog(data[0], draw: false);
-    }
-    if (data[3] == data[4] &&
+    } else if (data[3] == data[4] &&
         data[4] == data[5] &&
         data[3] == data[5] &&
         data[3] != '') {
       _showMyDialog(data[3], draw: false);
-    }
-    if (data[6] == data[7] &&
+    } else if (data[6] == data[7] &&
         data[7] == data[8] &&
         data[6] == data[8] &&
         data[6] != '') {
       _showMyDialog(data[6], draw: false);
-    }
-    if (data[0] == data[4] &&
+    } else if (data[0] == data[4] &&
         data[4] == data[8] &&
         data[0] == data[8] &&
         data[0] != '') {
       _showMyDialog(data[0], draw: false);
-    }
-    if (data[2] == data[4] &&
+    } else if (data[2] == data[4] &&
         data[4] == data[6] &&
         data[2] == data[6] &&
         data[2] != '') {
       _showMyDialog(data[2], draw: false);
-    }
-    if (data[0] == data[3] &&
+    } else if (data[0] == data[3] &&
         data[3] == data[6] &&
         data[0] == data[6] &&
         data[0] != '') {
       _showMyDialog(data[0], draw: false);
-    }
-    if (data[1] == data[4] &&
+    } else if (data[1] == data[4] &&
         data[4] == data[7] &&
         data[1] == data[7] &&
         data[1] != '') {
       _showMyDialog(data[1], draw: false);
-    }
-    if (data[2] == data[5] &&
+    } else if (data[2] == data[5] &&
         data[5] == data[8] &&
         data[2] == data[8] &&
         data[2] != '') {
       _showMyDialog(data[2], draw: false);
-    } else {
-      timesPLayed >= 8 ? _showMyDialog('O', draw: true) : null;
+    } else if (timesPLayed >= 8) {
+      _showMyDialog('O', draw: true);
     }
   }
 
@@ -106,8 +99,8 @@ class _HomePageState extends State<HomePage> {
       ];
     });
 
-    timesPLayed = 0;
     currentPlayerX = true;
+    timesPLayed = 0;
   }
 
   Future<void> _showMyDialog(String player, {required bool draw}) async {
